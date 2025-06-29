f5c090aff81bab4ac65ce6b023685d1c8d02f63d
Hubert Stanowski <hubert.stanowski@gmail.com>
2025-06-29 20:28:50 +0100

    Clean up navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import Navbar from "./components/navbar";
+import { Navbar } from "./components/navbar";
 import Playground from "./components/simulation";
 import { SimulationProvider } from "./contexts";
 
