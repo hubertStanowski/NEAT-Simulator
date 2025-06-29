e6952e64c036bfd56062e6a54ce7d794898f0a6b
Hubert Stanowski <hubert.stanowski@gmail.com>
2025-06-29 20:00:39 +0100

    Update type packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/neat": path.resolve(__dirname, "./src/neat"),
-      "@/snake": path.resolve(__dirname, "./src/snake"),
+      "@/neat": path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        "./src/neat",
+      ),
+      "@/snake": path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        "./src/snake",
+      ),
     },
   },
 });
