83d1b6e29d1e755382d0da7a482f6a6741de1849
Hubert Stanowski <hubert.stanowski@gmail.com>
2025-09-27 13:05:47 +0100

    Add pre-commit hooks for linting

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # NEAT-Simulator
 
 ### In progress - transfering my python NEAT simulations into the web interface.
+
 https://neat-simulator.netlify.app/
 
 ## Content
+
 - SnakeAI (transfered) - https://github.com/hubertStanowski/SnakeAI
 - FlappyBirdAI (not transfered) - https://github.com/hubertStanowski/FlappyBirdAI
--- src/snake/visuals.ts
+++ src/snake/visuals.ts
@@ -6,30 +6,42 @@ export const hsvToRgb = (
   let r = 1,
     g = 1,
     b = 1;
-  let i = Math.floor(h * 6);
-  let f = h * 6 - i;
-  let p = v * (1 - s);
-  let q = v * (1 - f * s);
-  let t = v * (1 - (1 - f) * s);
+  const i = Math.floor(h * 6);
+  const f = h * 6 - i;
+  const p = v * (1 - s);
+  const q = v * (1 - f * s);
+  const t = v * (1 - (1 - f) * s);
 
   switch (i % 6) {
     case 0:
-      (r = v), (g = t), (b = p);
+      r = v;
+      g = t;
+      b = p;
       break;
     case 1:
-      (r = q), (g = v), (b = p);
+      r = q;
+      g = v;
+      b = p;
       break;
     case 2:
-      (r = p), (g = v), (b = t);
+      r = p;
+      g = v;
+      b = t;
       break;
     case 3:
-      (r = p), (g = q), (b = v);
+      r = p;
+      g = q;
+      b = v;
       break;
     case 4:
-      (r = t), (g = p), (b = v);
+      r = t;
+      g = p;
+      b = v;
       break;
     case 5:
-      (r = v), (g = p), (b = q);
+      r = v;
+      g = p;
+      b = q;
       break;
   }
 
