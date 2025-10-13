0350d506b42017185aa7670e5a03c089c823011f
Hubert Stanowski <hubert.stanowski@gmail.com>
2025-10-13 21:27:26 +0100

    Run formatter on all files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import { githubURL } from '@/constants';
 
 const Footer = () => {
   return (
-    <footer className="c-space flex flex-wrap items-center justify-between border-t border-black-300 py-[clamp(0.5rem,1vh,1rem)]">
-      <p className="text-[clamp(0.75rem,1.5vh,1rem)] text-white-500">
+    <footer className="c-space border-black-300 flex flex-wrap items-center justify-between border-t py-[clamp(0.5rem,1vh,1rem)]">
+      <p className="text-white-500 text-[clamp(0.75rem,1.5vh,1rem)]">
         © 2025 Hubert Stanowski. All rights reserved.
       </p>
 
       <a href={githubURL} target="_blank" rel="noreferrer">
-        <SiGithub className="highlight text-[clamp(1.5rem,3vh,2.5rem)] text-white-500" />
+        <SiGithub className="highlight text-white-500 text-[clamp(1.5rem,3vh,2.5rem)]" />
       </a>
     </footer>
   );
--- src/components/simulation/Status.tsx
+++ src/components/simulation/Status.tsx
@@ -117,7 +117,7 @@ const Status = () => {
       {humanPlaying ? (
         <>
           <div className="flex-1"></div>
-          <div className="flex-2 flex flex-col justify-start space-y-[clamp(0.25rem,1vh,0.75rem)]">
+          <div className="flex flex-2 flex-col justify-start space-y-[clamp(0.25rem,1vh,0.75rem)]">
             <div className="text-3xl">Score: {score}</div>
             <div className="text-3xl text-purple-700">
               AI Generations: {currentGeneration}
